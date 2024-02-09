76f3603e3ca98da05df70f5ec4e990098e740500
mtthw <matbalinton@gmail.com>
2024-02-09 10:29:07 +0800

    Add homepage

---
+++ pages/home.php
@@ -0,0 +1,138 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div style="width: 1440px; height: 947px; position: relative; background: #F6F3E9">
+  <img style="width: 1140px; height: 763px; left: -251px; top: 369px; position: absolute" src="https://via.placeholder.com/1140x763" />
+  <div style="width: 52px; height: 52px; left: 365px; top: 383px; position: absolute; background: linear-gradient(312deg, rgba(16.98, 145.56, 142.85, 0.47) 0%, rgba(16.98, 145.56, 142.85, 0) 100%); border-radius: 9999px"></div>
+  <div style="width: 809px; height: 773.06px; left: 511px; top: -636.81px; position: absolute; border: 5px #F4EFDD solid"></div>
+  <div style="width: 904px; height: 808.82px; left: 1027px; top: 166.21px; position: absolute; border: 5px #F4EFDD solid"></div>
+  <div style="width: 1027px; height: 873.56px; left: -44px; top: -88.90px; position: absolute; border: 5px #F4EFDD solid"></div>
+  <div style="left: 352px; top: 196px; position: absolute; color: #11928F; font-size: 107.79px; font-family: Abyssinica SIL; font-weight: 400; word-wrap: break-word">memorable</div>
+  <div style="left: 820px; top: 59px; position: absolute; color: #121212; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">About us</div>
+  <div style="left: 958px; top: 59px; position: absolute; color: #121212; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">Contact</div>
+  <div style="left: 582px; top: 59px; position: absolute; color: #121212; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">Recommended Places</div>
+  <div style="width: 411px; left: 80px; top: 222px; position: absolute; color: #121212; font-size: 62px; font-family: Acme; font-weight: 400; word-wrap: break-word">Hotel for moments rich in emotions</div>
+  <div style="width: 406px; left: 80px; top: 468px; position: absolute"><span style="color: #909090; font-size: 24px; font-family: Acme; font-weight: 400; word-wrap: break-word">We have</span><span style="color: #FF6A28; font-size: 24px; font-family: Acme; font-weight: 400; word-wrap: break-word"> 459</span><span style="color: #909090; font-size: 24px; font-family: Acme; font-weight: 400; word-wrap: break-word"> rooms spread throuout Mindoro with room standards equivalent to 5 star hotels.</span></div>
+  <div style="width: 224px; height: 46px; left: 1135px; top: 45px; position: absolute">
+    <div style="width: 137px; height: 46px; left: 87px; top: 0px; position: absolute; background: #FF6A28; border-radius: 11px"></div>
+    <div style="left: 133px; top: 14px; position: absolute; color: white; font-size: 16px; font-family: Acme; font-weight: 400; letter-spacing: 0.96px; word-wrap: break-word">Login</div>
+    <div style="left: 0px; top: 14px; position: absolute; color: black; font-size: 16px; font-family: Inter; font-weight: 700; letter-spacing: 0.96px; word-wrap: break-word">Register</div>
+  </div>
+  <img style="width: 944px; height: 632px; left: 496px; top: 102px; position: absolute" src="https://via.placeholder.com/944x632" />
+  <div style="width: 160px; height: 55.78px; left: 1129px; top: 281.11px; position: absolute">
+    <div style="width: 160px; height: 55.78px; left: 0px; top: 0px; position: absolute; background: #6B3015; border-radius: 8.07px"></div>
+    <div style="width: 36.70px; height: 36.70px; left: 5.87px; top: 9.54px; position: absolute; opacity: 0.40; background: white; border-radius: 9999px"></div>
+    <div style="width: 23.49px; height: 23.49px; left: 12.48px; top: 16.15px; position: absolute; opacity: 0.70; background: white; border-radius: 9999px"></div>
+    <div style="width: 17.61px; height: 17.61px; padding-left: 5.14px; padding-right: 5.14px; padding-top: 2.20px; padding-bottom: 2.20px; left: 15.41px; top: 19.08px; position: absolute; justify-content: center; align-items: center; display: inline-flex">
+      <div style="width: 7.34px; height: 13.21px; background: rgba(112.62, 77.27, 15.02, 0.44)"></div>
+    </div>
+    <div style="left: 49.91px; top: 13.21px; position: absolute; color: white; font-size: 12.48px; font-family: Acme; font-weight: 400; letter-spacing: 0.12px; word-wrap: break-word">Get personalised<br/>offers now!!</div>
+  </div>
+  <div style="width: 218px; height: 76px; left: 1100px; top: 257px; position: absolute">
+    <div style="width: 218px; height: 76px; left: 0px; top: 0px; position: absolute; background: #FC6A8D; border-radius: 11px"></div>
+    <div style="width: 50px; height: 50px; left: 8px; top: 13px; position: absolute; opacity: 0.40; background: white; border-radius: 9999px"></div>
+    <div style="width: 32px; height: 32px; left: 17px; top: 22px; position: absolute; opacity: 0.70; background: white; border-radius: 9999px"></div>
+    <div style="width: 24px; height: 24px; padding-left: 7px; padding-right: 7px; padding-top: 3px; padding-bottom: 3px; left: 21px; top: 26px; position: absolute; justify-content: center; align-items: center; display: inline-flex">
+      <div style="width: 10px; height: 18px; background: #FC6A8D"></div>
+    </div>
+    <div style="left: 68px; top: 18px; position: absolute; color: white; font-size: 17px; font-family: Acme; font-weight: 400; letter-spacing: 0.17px; word-wrap: break-word">Get personalised<br/>offers now!!</div>
+  </div>
+  <div style="width: 118px; height: 118px; left: 705px; top: 294px; position: absolute">
+    <div style="width: 118px; height: 118px; left: 0px; top: 0px; position: absolute">
+      <div style="width: 118px; height: 118px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 9999px"></div>
+      <img style="width: 308.81px; height: 262.78px; left: -8.37px; top: -48.54px; position: absolute" src="https://via.placeholder.com/309x263" />
+    </div>
+    <div style="width: 20.09px; height: 20.09px; padding-left: 3.35px; padding-right: 3.35px; padding-top: 4.18px; padding-bottom: 4.18px; left: 33.31px; top: 16.16px; position: absolute; transform: rotate(3.56deg); transform-origin: 0 0; justify-content: center; align-items: center; display: inline-flex">
+      <div style="width: 13.39px; height: 11.72px; background: #244CEC; border-radius: 0.84px"></div>
+    </div>
+    <div style="width: 56.07px; height: 19.25px; left: 31.80px; top: 87.87px; position: absolute; background: #244CEC; border-radius: 5.02px"></div>
+    <div style="left: 41.84px; top: 92.06px; position: absolute; color: white; font-size: 9.21px; font-family: Acme; font-weight: 400; letter-spacing: 0.09px; word-wrap: break-word">Location</div>
+  </div>
+  <div style="width: 118px; height: 118px; left: 705px; top: 294px; position: absolute">
+    <div style="width: 118px; height: 118px; left: 0px; top: 0px; position: absolute">
+      <div style="width: 118px; height: 118px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 9999px"></div>
+      <div style="width: 118px; height: 118px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 9999px"></div>
+      <img style="width: 308.81px; height: 262.78px; left: -8.37px; top: -48.54px; position: absolute; opacity: 0.10" src="https://via.placeholder.com/309x263" />
+    </div>
+    <div style="width: 20.09px; height: 20.09px; padding-left: 3.35px; padding-right: 3.35px; padding-top: 4.18px; padding-bottom: 4.18px; left: 33.31px; top: 16.16px; position: absolute; transform: rotate(3.56deg); transform-origin: 0 0; justify-content: center; align-items: center; display: inline-flex">
+      <div style="width: 13.39px; height: 11.72px; background: #5F7FFF; border-radius: 0.84px"></div>
+    </div>
+    <div style="width: 56.07px; height: 19.25px; left: 31.80px; top: 87.87px; position: absolute; background: #5F7FFF; border-radius: 5.02px"></div>
+    <div style="left: 41.84px; top: 92.06px; position: absolute; color: white; font-size: 9.21px; font-family: Acme; font-weight: 400; letter-spacing: 0.09px; word-wrap: break-word">Location</div>
+  </div>
+  <div style="width: 186px; height: 69px; left: 80px; top: 581px; position: absolute">
+    <div style="width: 172px; height: 52px; left: 14px; top: 9px; position: absolute; background: #FF6A28; border-radius: 26px"></div>
+    <div style="width: 69px; height: 69px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 10px 4px 14px rgba(0, 0, 0, 0.13); border-radius: 9999px"></div>
+    <div style="width: 84px; left: 80px; top: 26px; position: absolute; color: white; font-size: 16px; font-family: Acme; font-weight: 400; word-wrap: break-word">Take a tour</div>
+    <div style="width: 44px; height: 44px; padding-top: 13.04px; padding-bottom: 13.04px; padding-left: 12.83px; padding-right: 12.79px; left: 14px; top: 13px; position: absolute; justify-content: center; align-items: center; display: inline-flex">
+      <div style="width: 18.37px; height: 17.93px; background: #FF6A28"></div>
+    </div>
+  </div>
+  <div style="width: 1360px; height: 246px; left: 80px; top: 734px; position: absolute; background: white; border-top-left-radius: 108px"></div>
+  <div style="width: 220px; height: 104px; left: 175px; top: 787px; position: absolute">
+    <div style="width: 97px; height: 24px; left: 0px; top: 0px; position: absolute; opacity: 0.60">
+      <div style="width: 24px; height: 24px; padding-top: 4px; padding-bottom: 4.10px; padding-left: 5px; padding-right: 5px; left: 0px; top: 0px; position: absolute; opacity: 0.30; justify-content: center; align-items: center; display: inline-flex">
+        <img style="width: 14px; height: 15.90px" src="https://via.placeholder.com/14x16" />
+      </div>
+      <div style="left: 36px; top: 1px; position: absolute; color: black; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">Location</div>
+    </div>
+    <div style="width: 220px; left: 0px; top: 42px; position: absolute; color: #121212; font-size: 24px; font-family: Abhaya Libre SemiBold; font-weight: 600; word-wrap: break-word">Puerto Galera</div>
+    <div style="width: 220px; left: 0px; top: 80px; position: absolute; opacity: 0.30; color: #121212; font-size: 19px; font-family: Acme; font-weight: 400; word-wrap: break-word">Oriental Mindoro</div>
+  </div>
+  <div style="width: 226px; height: 104px; left: 469px; top: 785px; position: absolute">
+    <div style="width: 86px; height: 24px; left: 0px; top: 0px; position: absolute; opacity: 0.60">
+      <div style="width: 24px; height: 24px; padding-top: 3px; padding-bottom: 2px; padding-left: 3.31px; padding-right: 3.31px; left: 0px; top: 0px; position: absolute; opacity: 0.30; flex-direction: column; justify-content: center; align-items: center; gap: 2px; display: inline-flex">
+        <div style="width: 10px; height: 10px; background: #121212; border-radius: 9999px"></div>
+        <div style="width: 17.39px; height: 7px; background: #121212"></div>
+      </div>
+      <div style="left: 36px; top: 1px; position: absolute; color: black; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">Guests</div>
+    </div>
+    <div style="width: 220px; left: 6px; top: 42px; position: absolute; color: #121212; font-size: 24px; font-family: Abhaya Libre SemiBold; font-weight: 600; word-wrap: break-word">3 Person</div>
+    <div style="width: 220px; left: 6px; top: 80px; position: absolute; opacity: 0.30; color: #121212; font-size: 19px; font-family: Acme; font-weight: 400; word-wrap: break-word">2 Adult, 1 Child</div>
+  </div>
+  <div style="width: 234px; height: 104px; left: 703px; top: 787px; position: absolute">
+    <div style="width: 98px; height: 24px; left: 0px; top: 0px; position: absolute; opacity: 0.60">
+      <div style="width: 24px; height: 24px; padding-top: 6.50px; padding-bottom: 3px; padding-left: 4px; padding-right: 4px; left: 0px; top: 0px; position: absolute; transform: rotate(90deg); transform-origin: 0 0; opacity: 0.30; flex-direction: column; justify-content: center; align-items: center; gap: 2.50px; display: inline-flex">
+        <div style="width: 4px; height: 0px; transform: rotate(90deg); transform-origin: 0 0; border: 2px #121212 solid"></div>
+        <img style="width: 16px; height: 8px" src="https://via.placeholder.com/16x8" />
+      </div>
+      <div style="left: 36px; top: 1px; position: absolute; color: black; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">Check-in</div>
+    </div>
+    <div style="width: 220px; left: 14px; top: 42px; position: absolute; color: #121212; font-size: 24px; font-family: Abhaya Libre SemiBold; font-weight: 600; word-wrap: break-word">4 February 2024</div>
+    <div style="width: 220px; left: 14px; top: 80px; position: absolute; opacity: 0.30; color: #121212; font-size: 19px; font-family: Acme; font-weight: 400; word-wrap: break-word">Select date</div>
+  </div>
+  <div style="width: 224px; height: 104px; left: 979px; top: 787px; position: absolute">
+    <div style="width: 109px; height: 24px; left: 0px; top: 0px; position: absolute; opacity: 0.60">
+      <div style="width: 24px; height: 24px; padding-top: 5px; padding-bottom: 3px; padding-left: 4px; padding-right: 4px; left: 24px; top: 0px; position: absolute; transform: rotate(90deg); transform-origin: 0 0; opacity: 0.30; flex-direction: column; justify-content: center; align-items: center; gap: 1.50px; display: inline-flex">
+        <div style="width: 6.50px; height: 10px; transform: rotate(90deg); transform-origin: 0 0; border: 2px #121212 solid"></div>
+        <img style="width: 16px; height: 8px" src="https://via.placeholder.com/16x8" />
+      </div>
+      <div style="left: 36px; top: 1px; position: absolute; color: black; font-size: 18px; font-family: Acme; font-weight: 400; word-wrap: break-word">Check-out</div>
+    </div>
+    <div style="width: 220px; left: 4px; top: 42px; position: absolute; color: #121212; font-size: 24px; font-family: Abhaya Libre SemiBold; font-weight: 600; word-wrap: break-word">7 February 2024</div>
+    <div style="width: 220px; left: 4px; top: 80px; position: absolute; opacity: 0.30; color: #121212; font-size: 19px; font-family: Acme; font-weight: 400; word-wrap: break-word">Select date</div>
+  </div>
+  <div style="width: 113px; height: 113px; left: 1247px; top: 785px; position: absolute; background: #FF6A28; border-radius: 15px"></div>
+  <div style="width: 63px; height: 67px; left: 1272px; top: 809px; position: absolute">
+    <div style="width: 34px; height: 34px; left: 9px; top: 0px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+      <div style="width: 22.67px; height: 22.67px; border: 3px white solid"></div>
+      <div style="width: 6.16px; height: 6.16px; border: 3px white solid"></div>
+    </div>
+    <div style="width: 63px; left: 0px; top: 42px; position: absolute; color: white; font-size: 20px; font-family: Acme; font-weight: 400; word-wrap: break-word">search</div>
+  </div>
+  <div style="width: 78px; height: 42px; left: 68px; top: 28px; position: absolute">
+    <div style="left: 0px; top: 0px; position: absolute; text-align: center; color: #262626; font-size: 32px; font-family: Inika; font-weight: 700; line-height: 20.58px; word-wrap: break-word">Hotel<br/>Logo</div>
+    <div style="width: 10px; height: 0px; left: 39px; top: 10px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 7px #65AEF2 solid"></div>
+  </div>
+</div>
+</body>
+</html>
