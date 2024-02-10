c25e2c3ebbeef4d748940fd6700f5c31c0b8d1ae
mtthw <matbalinton@gmail.com>
2024-02-10 17:53:18 +0800

    Fix error in tailwind

--- pages/home.php
+++ pages/home.php
@@ -1,15 +1,13 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../public/output.css">
 </head>
 <body>
-    
+    <button>ads</button>
+
 </body>
 </html>
--- public/homee.php
+++ public/homee.php
@@ -8,11 +8,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="../public/output.css" rel="stylesheet">
+    <link href="output.css" rel="stylesheet">
+
 </head>
 <body>
-    <h1 class=" bg-slate-500 text-cyan-400">das</h1>
-    <h1 class = " bg-red-500">dasda</h1>
-    <h2 class= " bg-black">das</h2>
+       <button>sfsd</button>
 </body>
 </html>
