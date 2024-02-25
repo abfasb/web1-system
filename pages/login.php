72668b8138415a91ee9788a15785e47633ffe523
mtthw <matbalinton@gmail.com>
2024-02-25 15:33:03 +0800

    Firebase done.

--- pages/login.php
+++ pages/login.php
@@ -36,19 +36,20 @@
     <title>Document</title>
     <link rel="stylesheet" href="../public/output.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Loopple/loopple-public-assets@main/motion-tailwind/motion-tailwind.css" rel="stylesheet">
-</head>
+    <script src="../scripts/GoogleAuth.js" defer type="module"></script>
+  </head>
 <body >
 <div class=" flex items-center justify-center bg-cover bg-center h-screen relative" style="background-image: url('../assets/img/hotel-bg.png');">
       <div class="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
       <div class="flex items-center justify-center px-10 w-full lg:p-12">
         <div class="flex items-center xl:p-10">
-          <form action = "./login.php" method = "POST" class="flex flex-col w-full h-full pb-6 px-12 text-center bg-white rounded-3xl">
+          <form action = "./login.php" method = "POST" class="flex flex-col w-full h-full pb-6 px-12 text-center bg-white rounded-3xl" autocomplete="off">
           <h3 class="mb-3 text-4xl font-extrabold text-dark-grey-900 pt-5">Sign In</h3>
             <p class="mb-4 text-grey-700">Enter your email and password</p>
-            <a class="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
+            <button autocomplete = "off" id="signInWithGoogle" class="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
               <img class="h-5 mr-2" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png" alt="">
               Sign in with Google
-            </a>
+            </button>
             <div class="flex items-center mb-3">
               <hr class="h-0 border-b border-solid border-grey-500 grow">
               <p class="mx-4 text-grey-600">or</p>
@@ -67,9 +68,9 @@
                 </div>
                 <span class="ml-3 text-sm font-normal text-grey-900">Keep me logged in</span>
               </label>
-              <a href="javascript:void(0)" class="mr-4 text-sm font-medium text-purple-blue-500">Forget password?</a>
+              <a href="#" class="mr-4 text-sm font-medium text-purple-blue-500">Forget password?</a>
             </div>
-            <button class="w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500">Sign In</button>
+            <input type="submit" value = "Submit" class="w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500">
             <p class="text-sm leading-relaxed text-grey-900">Not registered yet? <a href="./registration.php" class="font-bold text-grey-700">Create an Account</a></p>
           </form>
         </div>
--- scripts/GoogleAuth.js
+++ scripts/GoogleAuth.js
@@ -1,13 +1,33 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-    //gagawa pa lang
-};
+    apiKey: "AIzaSyCQbKvqlDHS6l0eJA73KFYyYmCgmV1BENw",
+    authDomain: "web1auth.firebaseapp.com",
+    projectId: "web1auth",
+    storageBucket: "web1auth.appspot.com",
+    messagingSenderId: "210242582176",
+    appId: "1:210242582176:web:f2cb5dd820a77a80807bc5",
+    measurementId: "G-5RE154X2TD"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  auth.languageCode = 'en';
+  const provider = new GoogleAuthProvider();
 
-const app = initializeApp(firebaseConfig);
-
-
-const auth = getAuth(app);
-
-
+  const googleLogin = document.getElementById("signInWithGoogle");
+  googleLogin.addEventListener('click', () => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const user = result.user;
+      console.log(user);
+      window.location.href = "../views/MainMenu.php";
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message
+      console.log(errorMessage);
+      // ...
+    });
+  })
