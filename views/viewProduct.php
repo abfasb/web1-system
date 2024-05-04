c9e75b084c70e31cd1dc3ece9d4c70c9e7685e7e
mtthw <matbalinton@gmail.com>
2024-05-04 21:29:12 +0800

    Add JSON in Upload Folder for img

--- views/viewProduct.php
+++ views/viewProduct.php
@@ -100,21 +100,21 @@ if (isset($_GET['product_id'])) {
           </label>
         </div>
 
-        <h2 class="mt-8 text-base text-gray-900">Choose subscription</h2>
+        <h2 class="mt-8 text-base text-gray-900">Choose Payment Method</h2>
         <div class="mt-3 flex select-none flex-wrap items-center gap-1">
           <label class="">
             <input type="radio" name="subscription" value="4 Months" class="peer sr-only" />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">4 Months</p>
+            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">Gcash</p>
             <span class="mt-1 block text-center text-xs">$80/mo</span>
           </label>
           <label class="">
             <input type="radio" name="subscription" value="8 Months" class="peer sr-only" checked />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">8 Months</p>
+            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">Cash On Delivery</p>
             <span class="mt-1 block text-center text-xs">$60/mo</span>
           </label>
           <label class="">
             <input type="radio" name="subscription" value="12 Months" class="peer sr-only" />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">12 Months</p>
+            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">Paypal</p>
             <span class="mt-1 block text-center text-xs">$40/mo</span>
           </label>
         </div>
